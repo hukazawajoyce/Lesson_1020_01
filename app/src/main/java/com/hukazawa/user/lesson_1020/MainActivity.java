f49b6612791227781641c21fe3c51ca739b9bdd5
hukazawajoyce <ck19910223@gmail.com>
2016-10-20 13:43:24 +0800

    Initial commit

---
+++ app/src/main/java/com/hukazawa/user/lesson_1020/MainActivity.java
@@ -0,0 +1,29 @@
+package com.hukazawa.user.lesson_1020;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1 (View v)
+    {
+        Intent it = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(it);
+    }
+
+    public void click2 (View v)
+    {
+        Intent it = new Intent();
+        it.setAction("call123");
+        startActivity(it);
+    }
+}
+
